9d71598d86e8c5665f87dcd492513ed2ec03cb42
clementlefevre <clement.san@gmail.com>
2019-09-13 17:58:11 +0200

    add time features on dataset

---
+++ todo.R
@@ -0,0 +1,15 @@
++ add week/weekend
++ add duration/distance on origianl data with exact coordinates()
++ remove mobike from data
++ add geocordinates of LOR /LOR district
++ add rounded hour
+
+
+- for the same bikeID  if  status "accessible"-> "accessible" : compute distance between the 2 status
+- #145 : bikeId change and no endtimestamp ????
+  - BVG : is the bike a subsiture to bad BVG connection (except bus)
+- airbnb map vs bike map
+- bike as intermodal transport : proportion of parked near trainstation/Ubahn/Sbahn
+- identify the moving pattern due to transport by car -> map of transportation by car.
+- nextbike/deezer : stations if free parl > extra $$
+- LIDL has virtual station.  
