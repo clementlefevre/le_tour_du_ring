c3e9fd05257d203769658484b56732ce2f701e14
clementlefevre <clement.san@gmail.com>
2019-09-14 00:46:49 +0200

    deckgl

--- todo.R
+++ todo.R
@@ -4,6 +4,11 @@
 + add geocordinates of LOR /LOR district
 + add rounded hour
 
+- modes split (%)
+        accessible          trip 
+        54.44399            45.55601 
+        
+- bikeId change = 0.5% of total events.
 
 - for the same bikeID  if  status "accessible"-> "accessible" : compute distance between the 2 status
 - #145 : bikeId change and no endtimestamp ????
@@ -11,5 +16,5 @@
 - airbnb map vs bike map
 - bike as intermodal transport : proportion of parked near trainstation/Ubahn/Sbahn
 - identify the moving pattern due to transport by car -> map of transportation by car.
-- nextbike/deezer : stations if free parl > extra $$
+- nextbike/deezer : stations if freeparl > extra $$
 - LIDL has virtual station.  
